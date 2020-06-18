063499546291cd042035e836ca51909e72ca997b
William Schwindt <williammschwindt@gmail.com>
2020-06-18 13:05:27 -0700

    added register options to home page

--- food-truck/src/components/Home.js
+++ food-truck/src/components/Home.js
@@ -8,7 +8,39 @@ const Home = () => {
             <div className="logo">
                 <ion-icon name="ios-cart" />
                 <h1>Food Truck</h1>
-                <p>Order from anywhere, enjoy meals in the comfort of your own home, easy as that.</p>
+                <p>Order from anywhere and enjoy meals in the comfort of your own home, easy as that.</p>
+            </div>
+            <div className="register">
+                <h2>Register</h2>
+                <div className="register-boxes">
+                    <div className="register-box">
+                        <h3>Deliverer</h3>
+                        <ul>
+                            <li>Receive notifications of pending orders</li>
+                            <li>Deliver groceries to customers</li>
+                            <li>Get paid</li>
+                        </ul>
+                        <a href="/register">Register</a>
+                    </div>
+                    <div className="register-box">
+                        <h3>Store</h3>
+                        <ul>
+                            <li>Receive customer orders</li>
+                            <li>Put grocery orders together for deliverers</li>
+                            <li>Confirm order number with deliverer</li>
+                        </ul>
+                        <a href="/register">Register</a>
+                    </div>
+                    <div className="register-box">
+                        <h3>Customer</h3>
+                        <ul>
+                            <li>Order grocierers from the Food Truck website</li>
+                            <li>Easy Pay online</li>
+                            <li>Groceries delivered right to your door</li>
+                        </ul>
+                        <a href="/register">Register</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
