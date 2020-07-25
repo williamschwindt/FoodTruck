d8a13d09227b420acb70b10c2c2dee014405185c
William Schwindt <williammschwindt@gmail.com>
2020-07-25 12:58:01 -0700

    displayed stores styled

--- food-truck/src/components/Store/Store/Store.js
+++ food-truck/src/components/Store/Store/Store.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Store = ({store}) => {
+    const [settings, setSettings] = useState(false)
+
+    const toggleSettings = () => {
+        setSettings(!settings)
+    }
 
     const editStore = () => {
 
@@ -12,14 +17,27 @@ const Store = ({store}) => {
 
     return(
         <div className="store-box">
-            <div className="store-settings-icon">
-                <ion-icon name="ios-settings" />
-            </div>
-            <h1>{store.store_name}</h1>
-            <div className="store-buttons">
-                <h2 onClick={editStore} id="store-edit">Edit</h2>
-                <h2 onClick={deleteStore} id="store-remove">Remove</h2>
-            </div>
+            {settings ?
+                <div>
+                    <div onClick={toggleSettings} className="store-settings-icon">
+                        <ion-icon name="ios-close" />
+                    </div>
+                    <h1>{store.store_name}</h1>
+                    <div className="store-buttons">
+                        <h2 onClick={editStore} id="store-edit">Edit</h2>
+                        <h2 onClick={deleteStore} id="store-remove">Remove</h2>
+                    </div>
+                </div>
+                :
+                <div>
+                    <div onClick={toggleSettings} className="store-settings-icon">
+                        <ion-icon name="ios-settings" />
+                    </div>
+                    <h1>{store.store_name}</h1>
+                    <h2>{store.store_address}</h2>
+                    <h2>View Store</h2>
+                </div>
+            }
         </div>
     )
 }
